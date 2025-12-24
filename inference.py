6aabd54e37934d50d1067b299cf0e06094c3a71f
amyhi10789 <awang28@eastsideprep.org>
2025-12-24 12:10:44 -0800

    g

--- inference.py
+++ inference.py
@@ -6,9 +6,9 @@ TOKEN = "cooper_person"
 
 NEGATIVE_PROMPT = (
     "child, teenager, female, different person, "
-    "cartoon, anime, large mustache, rustic vibe, illustration, painting, "
+    "cartoon, anime, large bushy thick mustache, rustic vibe, illustration, painting, "
     "overly smooth skin, plastic skin, airbrushed, wrinkles, sharp jawline"
-    "distorted face, asymmetrical eyes, extra facial features, latino man"
+    "distorted face, asymmetrical eyes, extra facial features, latino man, sharp facial features"
     "text, watermark, noise, black and white, military tough vibe, forehead wrinkles"
 )
 
@@ -28,9 +28,9 @@ def build_prompt(user_prompt: str) -> str:
         return (
             f"photo of {TOKEN}, adult man, lighter skin, head and shoulders portrait, "
             "light skin, short brown hair mostly covered by a dark blue cap, "
-            "trimmed beard, round face, straight and not sharp nose, medium eyebrows, small ears"
-            "neutral expression"
-            "indoor setting, soft natural lighting, realistic skin texture, "
+            "trimmed beard, round face, soft straight nose, medium eyebrows, small ears"
+            "neutral expression, medium resolution, soft facial features"
+            "indoor setting, soft natural lighting, blurry skin texture, "
         )
     else:
         return (
